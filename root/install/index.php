a68879548063b9803c29da9d56221dc8f81fad5d
Sajaki <sajaki9@gmail.com>
2012-05-05 13:05:03 +0200

    fix column type 'ignore_items_wow_id' to varchar
    it is referenced in phpbb_bbdkp_rt_temp_loot as varchar but in ignore
    table it is int. causes cast error in postgresql

--- root/install/index.php
+++ root/install/index.php
@@ -331,6 +331,10 @@ $versions = array(
 	
 	'1.0.0'  => array(
 	
+	'table_column_update' => array(
+		array($table_prefix . 'bbdkp_rt_ignoreitems', 'ignore_items_wow_id' =>  array('VCHAR', '')),
+		), 
+	
 		'custom' => array(
 			'raidtrackerupdater'
 		)
